b79f3f33b65d8f72d812bb526858c5f2e9b78122
Connor Hindley <connor@redhat.com>
2015-08-13 10:53:20 -0400

    skeleton - adopted from markdurrant/gulp-ink-email

---
+++ gulpfile.js
@@ -0,0 +1,77 @@
+'use strict';
+
+var gulp = require('gulp'),
+    path = require('path'),
+    livereload = require('gulp-livereload'),
+    sass = require('gulp-sass'),
+    runSequence = require('run-sequence'),
+    prefix = require('gulp-autoprefixer'),
+    inlineCss = require('gulp-inline-css'),
+    http = require('http'),
+    serveStatic = require('serve-static'),
+    finalhandler = require('finalhandler'),
+    rimraf = require('rimraf');
+
+var src = 'src/';
+var dist = path.resolve('dist/');
+
+gulp.task('server', function () {
+    var serve = serveStatic(dist);
+    var port = 4000;
+    http.createServer(function (req, res) {
+        var done = finalhandler(req, res);
+        serve(req, res, done);
+    }).listen(port);
+    console.log('\nlocal server runing at http://localhost:' + port + '/\n');
+});
+
+gulp.task('html', function () {
+    gulp.src(src + 'index.html')
+        .pipe(gulp.dest(dist))
+        .pipe(livereload());
+});
+
+gulp.task('css', function () {
+    gulp.src(src + 'sass/*.scss')
+        .pipe(sass({
+            outputStyle: ['expanded'],
+            errLogToConsole: true
+        }))
+        .pipe(prefix())
+        .pipe(gulp.dest(src))
+        .pipe(gulp.dest(dist))
+        .pipe(livereload());
+});
+
+gulp.task('inliner', function () {
+    return gulp.src(src + 'index.html')
+        .pipe(inlineCss())
+        .pipe(gulp.dest(dist));
+});
+
+gulp.task('deleteCss', function (cb) {
+    rimraf('./dist/styles.css', cb);
+});
+
+gulp.task('watch', function () {
+    livereload.listen(35729, function (err) {
+        if (err) {
+            return console.log(err);
+        }
+        gulp.watch(src + 'index.html', ['html']);
+        gulp.watch(src + 'sass/*.scss', ['css']);
+    });
+
+});
+
+gulp.task('build', function () {
+    runSequence('html', 'css');
+});
+
+gulp.task('prod', function () {
+    runSequence(['inliner', 'deleteCss']);
+});
+
+gulp.task('default', function (callback) {
+    runSequence('build', ['server', 'watch']);
+});
